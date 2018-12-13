43cd754b98da3243f6e216489c5a9756f4f0a1a1
Bryce Glover <RandomDSdevel@gmail.com>
2018-12-12 21:51:33 -0500

    openjfx:  `depends_on` Gradle v4.x instead of v5.x.
         The latter can't build OpenJFX due to some breaking changes between the two
    release series.

--- Formula/openjfx.rb
+++ Formula/openjfx.rb
@@ -23,7 +23,7 @@ class Openjfx < Formula
   depends_on "ant"
   depends_on "cmake" # For `javafx.web`.
   depends_on "gperf" # Also for `javafx.web`.
-  depends_on "gradle"
+  depends_on "gradle@4"
 
   depends_on :java => "11"
   depends_on :xcode => :build
