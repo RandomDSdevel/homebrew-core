50eea8b51ab83629f8342e1ff95ad9555e6d2aeb
Bryce Glover <RandomDSdevel@gmail.com>
2018-12-12 19:19:57 -0500

    gradle@4 4.10.3 (New formula.)

---
+++ Formula/gradle@4.rb
@@ -0,0 +1,25 @@
+class GradleAT4 < Formula
+  desc "Open-source build automation tool based on the Groovy and Kotlin DSL"
+  homepage "https://www.gradle.org/"
+  url "https://services.gradle.org/distributions/gradle-4.10.3-all.zip"
+  sha256 "336b6898b491f6334502d8074a6b8c2d73ed83b92123106bd4bf837f04111043"
+
+  bottle :unneeded
+
+  keg_only :versioned_formula
+
+  option "with-all", "Installs Javadoc, examples, and source in addition to the binaries"
+
+  depends_on :java => "1.7+"
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    libexec.install %w[bin lib]
+    libexec.install %w[docs media samples src] if build.with? "all"
+    (bin/"gradle").write_env_script libexec/"bin/gradle", Language::Java.overridable_java_home_env
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gradle --version")
+  end
+end
